e293b7d668895b30228eb04aded95fb041da63d8
Samuel López <samuel.lopez@uptc.edu.co>
2021-09-23 22:21:52 -0500

    trying to implement clientsocket

--- models/server.js
+++ models/server.js
@@ -12,15 +12,17 @@ class Server {
         this.port = PORT;
         this.server = require("http").createServer(this.app);
         this.io = require("socket.io")(this.server);
+        this.clientSocket = require("socket.io-client")('http://138.0.118.251:3000'); // This is a client connecting to the SERVER 2
         this.middleware();
         this.routes()
         this.askForHour()
         this.sockets();
+        this.anotherSockets();
     }
 
     askForHour() {
         axios.get('http://worldtimeapi.org/api/timezone/America/Bogota')
-        .then(function (response) {
+            .then(function (response) {
                 const hour_string = decodeHour(response.data.datetime.toString());
                 initTime(parseInt(hour_string[0]), parseInt(hour_string[1]), parseInt(hour_string[2]));
             }).catch(err => {
@@ -36,14 +38,24 @@ class Server {
 
     sockets() {
         this.io.on("connection", (socket) => {
-          setInterval(() => {
-            this.io.emit('time', {time: readTime()})
-          }, 1000)
-          socket.on('time_adjustment', function(data) {
-              updateTime(parseInt(data.hr), parseInt(data.mn), parseInt(data.sc));
-          })
+            setInterval(() => {
+                this.io.emit('time', { time: readTime() })
+            }, 1000)
+            socket.on('time_adjustment', function (data) {
+                updateTime(parseInt(data.hr), parseInt(data.mn), parseInt(data.sc));
+            })
         });
-      }
+    }
+
+    anotherSockets() {
+        this.clientSocket.on("connect", function () {
+            this.clientSocket.on('message', function (data) {
+                // We received a message from Server 2
+                // We are going to forward/broadcast that message to the "Lobby" room
+                this.clientSocket.emit("hello", {client: 'im your new client fcker'});
+            });
+        });
+    }
 
     routes() {
         this.app.use('/', require('../routes/monitor'));
